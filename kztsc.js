38677b3eed0605557f7c794b47fe733c262bb972
timigomen <121149519+timigomen@users.noreply.github.com>
2023-01-14 18:40:23 +0800

    Update kztsc.js

--- kztsc.js
+++ kztsc.js
@@ -21,7 +21,7 @@ if (window.console) {
   };
   console.log(string.makeMulti());
   
-  console.log('%c Wellcome to Charon Blog ', 'color: #f59f54 ;font-size: 35px;font-weight: bold;text-decoration: underline;');
+  console.log('%c Wellcome to GTC Blog ', 'color: #f59f54 ;font-size: 35px;font-weight: bold;text-decoration: underline;');
   
   console.log("%c Warning 警告! %c 你正在使用开发者控制台", "display: inline-block ; background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ","");
   console.log("%c Warning 警告! %c 如果你要使用本站代码", "display: inline-block ; background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 3px 7px 3px 7px ; border-radius: 3px 3px 3px 3px ","");
